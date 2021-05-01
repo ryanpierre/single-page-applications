d9a5455f8b0ca500c82fb37f98a3d1968aa842db
Ryan Pierre <info@ryanpierre.com>
2021-05-01 22:06:41 +0100

    make some small changes'

---
+++ 03/js/nominee-list.js
@@ -0,0 +1,16 @@
+class NomineeList {
+  constructor(nominees) {
+    this.list = nominees.map((name) => new Nominee(name));
+  }
+
+  toHTML() {
+    const listContainer = document.createElement("ul");
+    const listElements = this.list.map((nominee) => nominee.toHTML());
+
+    listElements.forEach((listElement) =>
+      listContainer.appendChild(listElement)
+    );
+
+    return listContainer;
+  }
+}
