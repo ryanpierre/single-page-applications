1ca0ef0204c8fac4b5c6e8cbdaca29ca2c9ef5db
Ryan Pierre <info@ryanpierre.com>
2021-05-04 21:21:39 +0100

    more nitpicks

--- example/public/index.js
+++ example/public/index.js
@@ -1,12 +1,8 @@
 async function onPageLoad() {
   console.log('Page Loaded');
 
-  const response = await fetch('http://localhost:3000/nominees', {
-    method: 'GET',
-  });
-
+  const response = await fetch('http://localhost:3000/nominees', { method: 'GET' });
   const { nominees } = await response.json();
-
   const nomineeList = new NomineeList(nominees.map((nominee) => new Nominee(nominee)));
 
   document.getElementById('listContainer').appendChild(nomineeList.toHTML());
